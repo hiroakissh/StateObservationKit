51fb4bf5abf93edc70d5ce8d782ab72a361083ea
Hiroaki Saito <48234687+hiroakissh@users.noreply.github.com>
2025-10-26 17:14:12 +0900

    Observation駆動ステートマシンのディスパッチ順序を保証

--- Sources/StateObservationKit/ObservationDrivenStateMachine.swift
+++ Sources/StateObservationKit/ObservationDrivenStateMachine.swift
@@ -1,43 +1,44 @@
 import Foundation
 #if canImport(Observation)
 import Observation
-
-@Observable
-public final class ObservationDrivenStateMachine<State: Equatable, Action> {
-    public private(set) var state: State
-    private let reducer: (inout State, Action) async -> Void
-
-    public init(initial: State, reducer: @escaping (inout State, Action) async -> Void) {
-        self.state = initial
-        self.reducer = reducer
-    }
-
-    public func dispatch(_ action: Action) {
-        Task {
-            var newState = state
-            await reducer(&newState, action)
-            let updatedState = newState
-            await MainActor.run { self.state = updatedState }
-        }
-    }
-}
-#else
-public final class ObservationDrivenStateMachine<State: Equatable, Action> {
-    public private(set) var state: State
-    private let reducer: (inout State, Action) async -> Void
-
-    public init(initial: State, reducer: @escaping (inout State, Action) async -> Void) {
-        self.state = initial
-        self.reducer = reducer
-    }
-
-    public func dispatch(_ action: Action) {
-        Task {
-            var newState = state
-            await reducer(&newState, action)
-            let updatedState = newState
-            await MainActor.run { self.state = updatedState }
-        }
-    }
-}
 #endif
+
+#if canImport(Observation)
+@Observable
+#endif
+public final class ObservationDrivenStateMachine<State: Equatable, Action> {
+    public private(set) var state: State
+#if canImport(Observation)
+    @ObservationIgnored
+#endif
+    private let reducerExecutor: ReducerExecutor<State, Action>
+
+    public init(initial: State, reducer: @escaping (inout State, Action) async -> Void) {
+        self.state = initial
+        self.reducerExecutor = ReducerExecutor(initial: initial, reducer: reducer)
+    }
+
+    public func dispatch(_ action: Action) {
+        Task {
+            let newState = await reducerExecutor.run(action: action)
+            await MainActor.run { self.state = newState }
+        }
+    }
+}
+
+private actor ReducerExecutor<State, Action> {
+    private var state: State
+    private let reducer: (inout State, Action) async -> Void
+
+    init(initial: State, reducer: @escaping (inout State, Action) async -> Void) {
+        self.state = initial
+        self.reducer = reducer
+    }
+
+    func run(action: Action) async -> State {
+        var nextState = state
+        await reducer(&nextState, action)
+        state = nextState
+        return state
+    }
+}
